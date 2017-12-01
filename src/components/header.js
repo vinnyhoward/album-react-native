a1ba48e38ae823cbac6911a596a7543257bc9a97
Vincent Howard <vincenguyenhoward@gmail.com>
2017-11-30 22:39:16 -0700

    First Commit

---
+++ src/components/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+  const { textStyle, viewStyle } = styles
+  return (
+    <View style={viewStyle}>
+  <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+}
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#551a8b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+  },
+  textStyle: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  }
+};
+
+
+export default Header;
